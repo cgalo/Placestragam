ad09c93ac6e981eebb5e67883941e944e2e68a86
cgalo <cgalo13@gmail.com>
2020-12-28 00:55:56 -0500

    Refactor SideDrawer component - Implemented portal location inside the index.html for the component

--- frontend/src/shared/components/Navigation/SideDrawer.tsx
+++ frontend/src/shared/components/Navigation/SideDrawer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './SideDrawer.css';
 
 const SideDrawer:React.FC<{}> = (props) => {
-    return (
-        <aside className="side-drawer">
-            {props.children}
-        </aside>
-    );
+    const content = <aside className="side-drawer"> {props.children} </aside>;
+    const drawerPortal = document.getElementById('drawer-hook');        // Look for the portal in the index.html
+
+    return drawerPortal ? ReactDOM.createPortal(content, drawerPortal) : null;
 }
 
 export default SideDrawer;
