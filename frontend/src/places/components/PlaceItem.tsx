b126eaabb59c6b9088b356aed616fb8a5c964445
cgalo <cgalo13@gmail.com>
2021-01-05 16:32:43 -0500

    Update PlaceItem component - Added logic to handle the delete button and request for a Place

--- frontend/src/places/components/PlaceItem.tsx
+++ frontend/src/places/components/PlaceItem.tsx
@@ -21,9 +21,24 @@ interface PlaceItemProp {
 const PlaceItem:React.FC<PlaceItemProp> = (props) => {
 
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
 
     const openMapHandler = () => setShowMap(true);
+
     const closeMapHandler = () => setShowMap(false);
+    
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true);
+    };
+
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false);
+    };
+
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false);
+        console.log("Deleting place...");
+    }
 
     return (
         <React.Fragment>
@@ -39,6 +54,33 @@ const PlaceItem:React.FC<PlaceItemProp> = (props) => {
                     <Map coordinates={props.coordinates} zoom={16}/>
                 </div>
             </Modal>
+            <Modal 
+                show={showConfirmModal}
+                onCancel={cancelDeleteHandler}
+                header="Are you sure?" 
+                footerClass="place-item__modal-actions"
+                footer={
+                    <React.Fragment>
+                        <Button 
+                            inverse
+                            onClick={cancelDeleteHandler}
+                        >
+                            CANCEL
+                        </Button>
+                        <Button 
+                            danger
+                            onClick={confirmDeleteHandler}
+                        >
+                            DELETE
+                        </Button>
+                    </React.Fragment>
+                }
+            >
+                <p>
+                    Do you want to proceed and delete this place? 
+                    Please note that this can be undone thereafter.
+                </p>
+            </Modal>
             <li className="place-item">
                 <Card className="place-item__content">
                     <div className="place-item__image">
@@ -53,7 +95,12 @@ const PlaceItem:React.FC<PlaceItemProp> = (props) => {
                         {/* Buttons to allow the user to interact with the 'Place' */}
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
                         <Button to={`/places/${props.id}`}>EDIT</Button>
-                        <Button danger>DELETE</Button>
+                        <Button 
+                            danger
+                            onClick={showDeleteWarningHandler}
+                        >
+                            DELETE
+                        </Button>
                     </div>
                 </Card>
             </li>
