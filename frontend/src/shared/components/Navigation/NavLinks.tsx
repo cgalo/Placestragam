3bf6078f0d244709314c7eda867248cb87bb462d
cgalo <cgalo13@gmail.com>
2021-01-06 17:14:29 -0500

    Applied Auth Context in NavLink.tsx - Applied context to show the NavLink depending if the user is signed in

--- frontend/src/shared/components/Navigation/NavLinks.tsx
+++ frontend/src/shared/components/Navigation/NavLinks.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks:React.FC<{}> = (props) => {
+    const auth = useContext(AuthContext);   // Get if the user is logged in
+
     return (
         <ul className="nav-links">
             <li>
@@ -12,21 +14,30 @@ const NavLinks:React.FC<{}> = (props) => {
                     ALL USERS
                 </NavLink>
             </li>
-            <li>
-                <NavLink to="/u1/places">
-                    MY PLACES
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/places/new">
-                    ADD PLACE
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/auth">
-                    AUTHENTICATE
-                </NavLink>
-            </li>
+            
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/u1/places">
+                        MY PLACES
+                    </NavLink>
+                </li>
+            )}
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/places/new">
+                        ADD PLACE
+                    </NavLink>
+                </li>
+            )}
+            
+            {!auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/auth">
+                        LOGIN/SIGNUP
+                    </NavLink>
+                </li>
+            )}
+
         </ul>
     );
 }
