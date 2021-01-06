35b310499727cb5f0e7b70837fe9c9777e778c18
cgalo <cgalo13@gmail.com>
2021-01-06 16:55:55 -0500

    Added SignUp option and fields in Auth.tsx - Added logic and fields to switch between logging and signup

--- frontend/src/user/pages/Auth.tsx
+++ frontend/src/user/pages/Auth.tsx
@@ -7,13 +7,14 @@ import Card from '../../shared/components/UIElements/Card';
 import { useForm } from '../../shared/hooks/form-hook';
 import { 
     VALIDATOR_MINLENGTH,
-    VALIDATOR_EMAIL
+    VALIDATOR_EMAIL,
+    VALIDATOR_REQUIRE
 } from '../../shared/util/validators';
 import './Auth.css';
 
 const Auth: React.FC<{}> = (props) => {
 
-    const [isLogged, setIsLogged] = useState(false);
+    const [isLoggingMode, setIsLoggingMode] = useState(true);
     const [formState, inputHandler, setFormData] = useForm ({
         email: {
             value: '',
@@ -25,26 +26,66 @@ const Auth: React.FC<{}> = (props) => {
         }
     }, false);
 
+    const switchModeHandler = () => {
+        // Switch between logging and signup mode
+        if (!isLoggingMode) {
+            // If we are in signup mode
+            setFormData({
+                ...formState.inputs,
+                first_name: undefined,
+                last_name: undefined
+            }, formState.inputs.email.isValid && formState.inputs.password.isValid);
+        } else {
+            // We are in loggin mode
+            setFormData({
+                ...formState.inputs,
+                first_name: {
+                    value: '',
+                    isValid: false
+                },
+                last_name: {
+                    value: '',
+                    isValid: false
+                }
+            }, false);
+        }
+        setIsLoggingMode(prevMode => !prevMode);    // Flip the mode
+    }
+
     const formSubmitHandler = (event:React.FormEvent) => {
         event.preventDefault();
         console.log("Submitting form...");
-        console.log(formState);
+        console.log(formState.inputs);
         
     };
-
-    if (isLogged) {
-        return (
-            <Card>
-                <h2>You are logged in.</h2>
-            </Card>
-        );
-    }
 
     return (
         <Card className="authentication">
             <h2>Login Required</h2>
             <hr />
             <form className="" onSubmit={formSubmitHandler}>
+                {!isLoggingMode && (
+                    <React.Fragment>
+                        <Input 
+                            id="first_name"
+                            element="input"
+                            type="text"
+                            label="First Name"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            onInput={inputHandler}
+                            errorText={"Please enter your first name."}
+                        />
+                        <Input 
+                            id="last_name"
+                            element="input"
+                            type="text"
+                            label="Last Name"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            onInput={inputHandler}
+                            errorText={"Please enter your last name."}
+                        />
+                    </React.Fragment>
+                )}
                 <Input 
                     id="email"
                     type="email"
@@ -65,9 +106,12 @@ const Auth: React.FC<{}> = (props) => {
                     errorText="Please enter a password with at least 6 characters."
                 />
                 <Button type="submit" disabled={!formState.isValid}>
-                    LOGIN
+                    {isLoggingMode ? 'LOGIN' : 'SIGNUP'}
                 </Button>
             </form>
+            <Button inverse onClick={switchModeHandler}>
+            SWITCH TO {isLoggingMode ? 'SIGNUP' : 'LOGIN'}
+            </Button>
         </Card>
     );
 }
