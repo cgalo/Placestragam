5fb1aaa3d4c8c080a971edd35b0a8d92c4ff6599
cgalo <cgalo13@gmail.com>
2021-01-12 17:35:20 -0500

    Completed location.ts - Added types from google maps inside the package - Created interface for the Geocode location request in util-types.ts - Finished logic for the location.ts to return http error or location

--- backend/util/location.ts
+++ backend/util/location.ts
@@ -1,12 +1,38 @@
 /**
- * Handle the API request to Google's Geolocation service to get
+ * Handle the API request to Google's Geocoding service to get
  * coordinates given an address
 */
 
+import axios from 'axios';
+
+import HttpError from '../models/http-error';
+import { Location } from '../types/places-types';
+import { GeoCodeResponsePayload } from '../types/util-types';
+
 const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
-async function getCoordsForAddress(address:String) {
+async function getCoordsForAddress(address:string) {
+    const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=
+        ${encodeURI(address)}
+        &key=${API_KEY}`
+    );
+    const data:GeoCodeResponsePayload = response.data;
     
+    if (!data || data.status === 'ZERO_RESULTS'){
+        // User entered valid address but it can't be found from Google
+        const message = "Could not find location for the specified address.";
+        const errorCode = 422;
+        throw new HttpError(message, errorCode);
+    }
+
+    const coordinates = data.results[0].geometry.location;  // Get coordinates from response
+    const location:Location = {                             // Convert response to Location object
+        latitude: coordinates.lat(),
+        longitude: coordinates.lng()
+    }
+
+    return location;                                        // Return final location object
 }
 
 export default getCoordsForAddress;
