711c6f8c8401ee9d45b751de9c9a59895685aa99
cgalo <cgalo13@gmail.com>
2021-01-12 17:05:50 -0500

    Created util/locations.ts - File will help in performing request to google's geolocation to get coordinates from a given address

---
+++ backend/util/location.ts
@@ -0,0 +1,12 @@
+/**
+ * Handle the API request to Google's Geolocation service to get
+ * coordinates given an address
+*/
+
+const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+async function getCoordsForAddress(address:String) {
+    
+}
+
+export default getCoordsForAddress;
