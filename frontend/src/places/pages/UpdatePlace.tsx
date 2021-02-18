72e9794d637db28a9fb83e177df245699874508f
Carlos Galo <cgalo13@gmail.com>
2021-02-18 16:05:16 -0500

    Updated the UpdatePlace page - Added the logic to perform PATCH request with the new place data - Added 'place' attribute in api-types interface - Updated the Input component to allow placeholder in textarea input types - Reduced code clutter inside the Inputs props interface - Removed unused import in schema-types.ts

--- frontend/src/places/pages/UpdatePlace.tsx
+++ frontend/src/places/pages/UpdatePlace.tsx
@@ -1,54 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
+// Components
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 
+// Hooks, utilities, interfaces and styling
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { useForm } from '../../shared/hooks/form-hook';
 import { 
     VALIDATOR_REQUIRE, 
     VALIDATOR_MINLENGTH 
 } from '../../shared/util/validators';
-import { useForm } from '../../shared/hooks/form-hook';
-import { Place } from '../../types/places-types';
+import { AuthContext } from '../../shared/context/auth-context';
+import type { Place } from '../../types/places-types';
 import './PlacesForm.css';
 
 interface ParamType {
     placeId: string;
+    userId: string;
 }
-const PLACES:Array<Place> = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famouse sky scrapers in the world!',
-        image:
-        'http://www.interestingamerica.com/images/NY_images/Manhattan/Empire_State_Building/ESB_Exterior_Sunset_Gary718_412_426.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1',
-        location: {
-            latitude: 40.7484,
-            longitude: -73.9857            
-        }
-    },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famouse sky scrapers in the world!',
-        image:
-        'http://www.interestingamerica.com/images/NY_images/Manhattan/Empire_State_Building/ESB_Exterior_Sunset_Gary718_412_426.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u2',
-        location: {
-            latitude: 40.7484,
-            longitude: -73.9857            
-        }
-    }
-];
 
 const UpdatePlace: React.FC<{}> = (props) => {
     const { placeId } = useParams<ParamType>();
-    const [isLoading, setIsLoading] = useState(true);
-
+    const [identifiedPlace, setIdentifiedPlace] = useState({} as Place);
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const history = useHistory();                   // Will assist in redirecting the user
+    const { userId } = useContext(AuthContext);    // Listener to context, contains userId
     const [formState, inputHandler, setFormData] = useForm ({
         title: {
             value: '',
@@ -60,10 +41,22 @@ const UpdatePlace: React.FC<{}> = (props) => {
         }
     }, false);
 
-    const identifiedPlace = PLACES.find(p => p.id === placeId);
-
-
     useEffect(() => {
+        const fetchPlace = async() => {
+            // Get the place object/data from the backend and set it as the 'identifiedPlace'
+            try {
+                const method = 'GET', url = `http://localhost:5000/api/places/${placeId}`;
+                const responseData = await sendRequest(url, method, null, {});
+                const place:Place = responseData.place || {} as Place;
+                if (place){
+                    setIdentifiedPlace(place);
+                }
+            }
+            catch(err){}
+        }
+
+        fetchPlace();
+
         if (identifiedPlace) {
             setFormData({
                 title: {
@@ -71,68 +64,86 @@ const UpdatePlace: React.FC<{}> = (props) => {
                     isValid: true
                 },
                 description: {
-                    value: identifiedPlace?.description,
-                    isValid: identifiedPlace?.title
+                    value: identifiedPlace.description,
+                    isValid: identifiedPlace.title
                 }
             }, true);
         }
-        setIsLoading(false);
-    }, [setFormData, identifiedPlace]);
+    }, [setFormData]);
+
+    const updatePlace = async() => {
+        try {
+            // Variables/data needed to perform the request
+            const method = 'PATCH', url = `http://localhost:5000/api/places/${placeId}`;
+            const body = JSON.stringify({
+                title: (formState.inputs.title.value === "") ? identifiedPlace.title : formState.inputs.title.value,
+                description: formState.inputs.description.value
+            });
+            const header = {'Content-Type' : 'application/json'};
+            
+            // Now we can perform the PATCH request
+            const responseData = await sendRequest<Place>(url, method, body, header);
+            const updatedPlace:Place = responseData.place || {} as Place;
+            
+            // If we got the updated place object from the backend
+            if (updatedPlace){
+                history.push('/');
+            }
+        } catch{
+            // Nothing to do here, the ErrorModal & useHttpClient hooks already handle the errors
+        }
+    }
 
     const formSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault();
-        console.log(formState.inputs);
-    }
-
-    if (!identifiedPlace){
-        return (
-            <div className="center">
-                <Card>
-                    <h2>Could not find place!</h2>
-                </Card>
-            </div>
-        );
-    }
-
-    if (isLoading) {
-        return (
-            <div className="center">
-                <Card>
-                    <h2>Loading...</h2>
-                </Card>
-            </div>
-        );
+        updatePlace();
     }
 
     return (
-    <form className="place-form" onSubmit={formSubmitHandler}>
-        <Input 
-            id="title"
-            element="input"
-            type="text"
-            label="Title"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid title."
-            onInput={inputHandler}
-            initValue={formState.inputs.title.value}
-            initValid={formState.inputs.title.isValid}
-        />
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError}/>
+            {isLoading && (<div className="center">
+                <LoadingSpinner asOverlay/>
+            </div>)}
 
-        <Input 
-            id="description"
-            element="textarea"
-            type="textarea"
-            label="Description"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid description (min. 5 length)."
-            onInput={inputHandler}
-            initValue={formState.inputs.description.value}
-            initValid={formState.inputs.description.isValid}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-            UPDATE PLACE
-        </Button>
-        </form>
+            {!isLoading && !identifiedPlace && (
+                <div className="center">
+                    <Card><h2>Could not find place!</h2></Card>
+                </div>
+            )}
+
+            {!isLoading && identifiedPlace && (
+                <form className="place-form" onSubmit={formSubmitHandler}>
+                    <Input 
+                        placeholder={identifiedPlace.title}
+                        id="title"
+                        element="input"
+                        type="text"
+                        label="Title"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter a valid title."
+                        onInput={inputHandler}
+                        initValue={formState.inputs.title.value}
+                        initValid={formState.inputs.title.isValid}
+                    />
+                    <Input 
+                        placeholder="New description here..."
+                        id="description"
+                        element="textarea"
+                        type="textarea"
+                        label="Description"
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        errorText="Please enter a valid description (min. 5 length)."
+                        onInput={inputHandler}
+                        initValue={formState.inputs.description.value}
+                        initValid={formState.inputs.description.isValid}
+                    />
+                    <Button type="submit" disabled={!formState.isValid}>
+                        UPDATE PLACE
+                    </Button>
+                </form>
+            )}
+        </React.Fragment>
     );
 }
 
--- frontend/src/types/api-types.ts
+++ frontend/src/types/api-types.ts
@@ -1,9 +1,11 @@
 // Interfaces from API responses
 import type { IUser } from './user-types';
+import type { Place } from './places-types';
 
 export interface IGetResponse<T> {
     user?: IUser;
     users?: Array<T>;
+    place?: Place;
     places?: Array<T>;
     message?: string;
 }
